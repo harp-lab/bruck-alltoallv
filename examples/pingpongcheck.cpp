e97414988cd7b21e268b082989013dd1fc752406
kokofan <koko14fan@gmail.com>
2022-07-29 17:19:08 -0500

    update

--- examples/pingpongcheck.cpp
+++ examples/pingpongcheck.cpp
@@ -7,7 +7,7 @@
 
 #include "../brucks.h"
 
-int ite_count = 100;
+int ite_count = 1;
 
 int main(int argc, char **argv)
 {
@@ -33,25 +33,36 @@ int main(int argc, char **argv)
     std::vector<double> times;
 
     for (int i = 0; i < ite_count; i++) {
-		int send_data = rank;
-		int receve_data = -1;
+    	for (int count = 2; count < 2048; count *= 2) {
 
-		double Start = MPI_Wtime();
-		if (rank == (nprocs-1))
-			MPI_Recv(&receve_data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		if (rank == 0)
-			MPI_Send(&send_data, 1, MPI_INT, (nprocs-1), 0, MPI_COMM_WORLD);
-		double end = MPI_Wtime();
-		double time = end - Start;
-		times.push_back(time);
+    		int send_data[count];
+    		std::fill_n(send_data, count, rank);
+
+			int receve_data[count];
+			std::fill_n(receve_data, count, -1);
+
+			double Start = MPI_Wtime();
+			if (rank == (nprocs-1))
+				MPI_Recv(&receve_data, count, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			if (rank == 0)
+				MPI_Send(&send_data, count, MPI_INT, (nprocs-1), 0, MPI_COMM_WORLD);
+			double end = MPI_Wtime();
+			double time = end - Start;
+			times.push_back(time);
+    	}
     }
 
+    int j = 0;
     for (int i = 0; i < ite_count; i++) {
-		double max_time = 0;
-		MPI_Allreduce(&times[i], &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    	for (int count = 2; count < 2048; count *= 2) {
 
-		if (times[i] == max_time)
-			std::cout << rank << " " << max_time << std::endl;
+    		double max_time = 0;
+			MPI_Allreduce(&times[j], &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+			if (times[j] == max_time)
+				std::cout << rank << " " << count << " " << max_time << std::endl;
+			j++;
+    	}
 	}
 
 
